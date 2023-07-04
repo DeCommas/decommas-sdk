490175503af6144bc15ac3aa3d29ecaed1d5d984
Iurii Kiselev <y.kiselev@3commas.io>
2023-07-04 11:04:01 +0300

    fixed test

--- src/address/mock.ts
+++ src/address/mock.ts
@@ -19,16 +19,20 @@ export const mockResponseRaw: TGetTokensResponseRaw = {
     }]
 }
 
-export const mockResponse: TGetTokensResponse = [{
-    chainName: "mainnet",
-    chainId: 1,
-    address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-    name: "Tether USD",
-    decimals: 6,
-    symbol: "USDT",
-    logoUrl: "https://assets.coingecko.com/coins/images/325/large/Tether.png?1668148663",
-    actualPrice: 0.999985,
-    isVerified: true,
-    isStable: true,
-    amount: 10000000
-}]
+export const mockResponse: TGetTokensResponse = {
+    count: 22,
+    status: 200,
+    result: [{
+        chainName: "mainnet",
+        chainId: 1,
+        address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+        name: "Tether USD",
+        decimals: 6,
+        symbol: "USDT",
+        logoUrl: "https://assets.coingecko.com/coins/images/325/large/Tether.png?1668148663",
+        actualPrice: 0.999985,
+        isVerified: true,
+        isStable: true,
+        amount: 10000000
+    }]
+}

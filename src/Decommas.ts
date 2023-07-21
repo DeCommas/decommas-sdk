760042ee0d9a7d22c08bd358eb30cb3cea81bc13
Iurii Kiselev <y.kiselev@3commas.io>
2023-07-21 11:24:47 +0300

    changed transaction namespace to tx

--- src/Decommas.ts
+++ src/Decommas.ts
@@ -6,13 +6,13 @@ import { Transaction } from "@business/transaction";
 export class Decommas {
   public readonly address: Address;
   public readonly metadata: Metadata;
-  public readonly transaction: Transaction;
+  public readonly tx: Transaction;
 
   constructor(apiKey?: string) {
     const httpRequest = new HttpRequest(apiKey);
 
     this.address = new Address(httpRequest);
     this.metadata = new Metadata(httpRequest);
-    this.transaction = new Transaction(httpRequest);
+    this.tx = new Transaction(httpRequest);
   }
 }
